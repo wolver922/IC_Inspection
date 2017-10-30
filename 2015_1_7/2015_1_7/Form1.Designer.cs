af595e3cf6fc2b6eb58f759f23bceb94e6d59d30
wolver922 <leo0938278619@gmail.com>
2017-10-30 14:12:09 +0800

    IC_Inspection

---
+++ 2015_1_7/2015_1_7/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace _2015_1_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "載入圖片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(511, 358);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(562, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(507, 358);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(562, 388);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "結果";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(699, 388);
+            this.trackBar1.Maximum = 7;
+            this.trackBar1.Minimum = 5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 56);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(810, 395);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(644, 396);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "敏感度";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(663, 438);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "一般設5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(756, 438);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "困難設7";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1081, 548);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ 2015_1_7/2015_1_7/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+
+namespace _2015_1_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Image myImage;
+        Image<Gray, byte> ooldImg;
+        private const int Height = 480, Width = 640;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                myImage = Image.FromFile(file.FileName);
+                pictureBox1.Image = myImage;
+                ooldImg = new Image<Gray, byte>((Bitmap)myImage);
+            }
+        }
+        class LineData
+        {
+            public double lineColor;
+            public int Y;
+            public LineData(int linecolor,int y)
+            {
+                lineColor = linecolor;
+                Y = y;
+            }
+            
+        };
+        int flag = 5;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Image<Bgr, Byte> oldImg = new Image<Bgr, Byte>(ooldImg.Bitmap);
+            Image<Gray, Byte> detImg = new Image<Gray, Byte>(Width, Height);
+            //Image<Gray, double> cvImageIntegral = new Image<Gray,double>(16,12);
+            //Image<Gray, Byte> arrMap = null;
+            Bitmap arrMap = new Bitmap(Width, Height);
+            Bitmap arrMap2 = new Bitmap(Width, Height);
+            detImg.Bitmap = oldImg.Bitmap;
+            List<LineData> myLine = new List<LineData>();
+            List<LineData> myLine2 = new List<LineData>();
+            //LineData lineData = new LineData();
+            for (int i = 0; i < 20;i++ )
+                detImg = detImg.SmoothGaussian(3);
+            double addlinecolor;
+            double addlinecolor2;
+           double[] lineColor = new double[480];
+           double[] lineColor2 = new double[480];
+           
+            //橫向加總
+            for (int y = 0; y < Height ; y++)
+            {
+                addlinecolor = 0;
+                addlinecolor2 = 0;
+                for (int x = 0; x < Width ; x++)
+                {
+                    if (x > Width / 2)
+                    {
+                        addlinecolor2 += Emgu.CV.CvInvoke.cvGetReal2D(detImg, y, x);
+                        if (x == Width - 1)
+                        {
+                            lineColor2[y] = (addlinecolor2 / 320);
+                            LineData thisline2 = new LineData((int)(addlinecolor / 320), y);
+                            myLine2.Add(thisline2);
+                            int gray = (int)(addlinecolor / 320);
+                            arrMap2.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                        }
+                    }
+                    else
+                    {
+                        addlinecolor += Emgu.CV.CvInvoke.cvGetReal2D(detImg, y, x);
+                        if (x == Width / 2)
+                        {
+                            lineColor[y] = (addlinecolor / 320);
+                            LineData thisline = new LineData((int)(addlinecolor / 320), y);
+                            myLine.Add(thisline);
+                            int gray = (int)(addlinecolor / 320);
+                            arrMap.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                        }
+                    }
+                }
+                /*lineColor[y] = (addlinecolor / 320);
+                LineData thisline = new LineData((int)(addlinecolor / 320),y);
+                myLine.Add(thisline);
+                int gray = (int)(addlinecolor / 320);*/
+                /*for (int i = 0; i < Width; i++)
+                    arrMap.SetPixel(i, y, Color.FromArgb(gray, gray, gray));*/
+            }
+            myLine.Sort((x, y) => { return -x.lineColor.CompareTo(y.lineColor); });
+            myLine2.Sort((x, y) => { return -x.lineColor.CompareTo(y.lineColor); });
+            double add = 0;
+            double topall = 0;
+            double alldown = 0;
+            int count = 0;
+            foreach (LineData aline in myLine)
+            {
+                count++;
+                if (count == myLine.Count / 2)
+                {
+                    add /= 240;
+                    topall = add;
+                    add = 0;
+                }
+                if (count == myLine.Count)
+                {
+                    add /= 240;
+                    alldown = add;
+                    add = 0;
+                }
+                add += aline.lineColor;
+                
+            }
+            double add2 = 0;
+            double topall2 = 0;
+            double alldown2 = 0;
+            foreach (LineData aline in myLine2)
+            {
+                count++;
+                if (count == myLine2.Count / 2)
+                {
+                    add2 /= 240;
+                    topall2 = add2;
+                    add2 = 0;
+                }
+                if (count == myLine2.Count)
+                {
+                    add2 /= 240;
+                    alldown2 = add2;
+                    add2 = 0;
+                }
+                add2 += aline.lineColor;
+
+            }
+            int rand1 = 5;
+            for (int y = 0 + rand1; y < Height - rand1; y++)
+            {
+                if(Math.Abs(lineColor[y] -alldown) > 10)
+                    for (int x = 0; x < Width; x++)
+                    {
+                        PointF point = new PointF(x, y);
+                        PointF point2 = new PointF(x, y);
+                        LineSegment2D line = new LineSegment2D(Point.Round(point), Point.Round(point2));
+                        oldImg.Draw(line, new Bgr(0, 0, 255), 2);
+                    }
+               /* if (Math.Abs(lineColor2[y] - alldown2) > flag)
+                    for (int x = (Width /2)+1; x < Width; x++)
+                    {
+                        PointF point = new PointF(x, y);
+                        PointF point2 = new PointF(x, y);
+                        LineSegment2D line = new LineSegment2D(Point.Round(point), Point.Round(point2));
+                        oldImg.Draw(line, new Bgr(0, 0, 255), 2);
+                    }*/
+            }
+            for (int y = 0 + rand1; y < Height - rand1; y++)
+            {
+                for (int x = 0 + rand1; x < Width - rand1; x++)
+                {
+                    if (x > Width / 2)
+                    {
+                        if (Math.Abs(lineColor2[y] - Emgu.CV.CvInvoke.cvGetReal2D(detImg, y, x)) > flag)
+                        {
+
+                            PointF point = new PointF(x, y);
+                            PointF point2 = new PointF(x, y);
+                            LineSegment2D line = new LineSegment2D(Point.Round(point), Point.Round(point2));
+                            oldImg.Draw(line, new Bgr(0, 0, 255), 2);
+
+                        }
+                    }
+                    else
+                    {
+                        if (Math.Abs(lineColor[y] - Emgu.CV.CvInvoke.cvGetReal2D(detImg, y, x)) > flag)
+                        {
+
+                            PointF point = new PointF(x, y);
+                            PointF point2 = new PointF(x, y);
+                            LineSegment2D line = new LineSegment2D(Point.Round(point), Point.Round(point2));
+                            oldImg.Draw(line, new Bgr(0, 0, 255), 2);
+
+                        }
+                    }
+                }
+            }
+            
+            
+            pictureBox2.Image = oldImg.Bitmap;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            flag = trackBar1.Value;
+            label1.Text = ""+trackBar1.Value;
+            //pictureBox2.Image = oldImg.Bitmap;
+        }
+    }
+}
